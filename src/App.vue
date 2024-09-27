8ea2e15681a2e9ad5de3c5ab1bad48db515ef349
kemalhiero <kemalhiero@gmail.com>
2024-09-27 20:32:58 +0700

    coba authenticated app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
 
-  <Navbar/>
+  <Navbar v-if="isLoggedOut"/>
     <RouterView/>
   <!-- <Footer/> -->
 
 </template>
 
 <script setup>
+
+  import { ref } from 'vue';
   import {RouterView } from 'vue-router';
   import Navbar from '@/components/Navbar.vue';
   import Footer from '@/components/Footer.vue';
+
+  // authenticated
+  import AuthenticatedHome from './views/authenticated/Index.vue';
+
+  // Status login (bisa diambil dari Pinia atau API)
+  const isLoggedOut = ref(false); // ganti false dengan status login aktual, misalnya dari Vuex atau API
 
 </script>
